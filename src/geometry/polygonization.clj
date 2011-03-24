36dd16a045b836c365f532f0da536dc3fa3c13cd
Andrew Moreland <andy@andymo.org>
2011-03-24 14:06:43 -0700

    Better graphics library

--- src/geometry/polygonization.clj
+++ src/geometry/polygonization.clj
@@ -2,23 +2,20 @@
 
 
 (defn sort-points-by-angle [ref-point points]
-  (sort-by (fn [[x y]] (Math/atan (float (/ (- y (:y ref-point)) (- x (:x ref-point)))))) points))
+  (sort-by (fn [{x :x y :y}] (Math/atan (float (/ (- y (:y ref-point)) (- x (:x ref-point)))))) points))
 
 (defn polygonalize-points [points]
   (let [points-by-x (reverse (sort-by :x points))
         rightmost-point (first points-by-x)
         rightmost-x (:x rightmost-point)
         rightmost-y (:y rightmost-point)
-        points-by-angle (cons (cons
-                               rightmost-point
-                               (sort-points-by-angle {:x rightmost-x :y rightmost-y} (rest points)))
-                              rightmost-point)]
-    (partition 4 2 (flatten points-by-angle))))
+        points-by-angle [rightmost-point (sort-points-by-angle {:x rightmost-x :y rightmost-y} (rest points-by-x)) rightmost-point]]
+    (partition 2 1 (flatten points-by-angle))))
 
 
 (defn -main []
   (let [my-panel (graph/panel)
-        points [{:x 10 :y 10} {:x 30 :y 20} {:x 100 :y 120} {:x 90 :y 60} {:x 95 :y 19 } {:x 90 :y 90} {:x 25 :y 195} {:x 144 :y 144}]]
+        points (take 15 (graph/random-points))]
     (graph/create-new-pane my-panel)
     (graph/update-drawing my-panel (fn [g]
                                (graph/draw-lines g (polygonalize-points points))
--- src/geometry/triangulation.clj
+++ src/geometry/triangulation.clj
@@ -1,3 +1,33 @@
+(ns geometry.triangulation (:import java.lang.Thread [java.util.concurrent ScheduledThreadPoolExecutor TimeUnit] [java.awt Color]) (:require [geometry.polygonization :as poly] [geometry.graphics :as graph]))
 
-(ns geometry.triangulation (:require [geometry.polygonization :as poly] [geometry.graphics :as graphi]))
+(defn slope [[{x1 :x y1 :y} {x2 :x y2 :y}]]
+  (if (not (= 0 (- x2 x1)))
+    (/ (- y2 y1) (- x2 x1))
+    10000000000))
 
+(defn collinear? [point-pair1 point-pair2]
+  (if (= (slope point-pair1) (slope point-pair2) (slope [(first point-pair1) (first point-pair2)]))
+    true false))
+
+(defn triangulate-points [points]
+  (let [points-by-y (reverse (sort-by :y points))
+        polygonized-points (poly/polygonalize-points points)
+        top-point (first points-by-y)n
+        lower-points (rest points-by-y)]
+    
+    (partition 2 1 (flatten lower-points))))
+
+(defn draw-function [g]
+  (let [points (take 15 (graph/random-points))]
+    (.setColor g (Color. 255 0 255))
+    (graph/draw-lines g (triangulate-points points))
+    (graph/draw-points g points)
+    (.setColor g (Color. 0 0 0))
+    (graph/draw-lines g (poly/polygonalize-points points))))
+
+(defn redraw [g]
+  (draw-function g))
+
+(defn -main [my-panel]
+  (doto (ScheduledThreadPoolExecutor. 1)
+    (.scheduleWithFixedDelay #(graph/update-drawing my-panel (fn [g] (redraw g))) 0 5000 TimeUnit/MILLISECONDS)))
